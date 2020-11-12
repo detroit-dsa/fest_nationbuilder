19f1815e3a8fe77c12fae8ef69e8d9914d355ebd
Andy Hall <ajhall.us@gmail.com>
2020-11-11 22:03:01 -0500

    Use tags for state storage

--- src/fest_nationbuilder/nationbuilder.py
+++ src/fest_nationbuilder/nationbuilder.py
@@ -1,20 +1,22 @@
 import datetime
+import json
 import urllib
-from pprint import pprint
-
-from fest_nationbuilder.utils import gcal_event_to_nationbuilder
 
 import requests
 from fest import utils
 from fest.google import GoogleCalendar
 from fest.utils import Future
 
+from fest_nationbuilder.utils import gcal_to_nb
+
+DIGEST_PREFIX = "gcal_event_digest:"
+EVENT_ID_PREFIX = "gcal_event_id:"
+
 
 class NationBuilder:
     def __init__(self, api_token, nation_slug, site_slug, calendar_id):
         self.logger = utils.logger(self)
         self.api_token = api_token
-        self.nation_slug = nation_slug
         self.site_slug = site_slug
         self.calendar_id = calendar_id
 
@@ -26,10 +28,9 @@ class NationBuilder:
     def iter_events(self, path=None, **kwargs):
         if not path:
             path = f"/api/v1/sites/{self.site_slug}/pages/events"
+        kwargs["calendar_id"] = self.calendar_id
 
         url = f"{self.nationbuilder_base_url}{path}"
-
-        kwargs["calendar_id"] = self.calendar_id
         params = urllib.parse.urlencode(kwargs)
         self.logger.info(f" GET {url}?{params}")
 
@@ -58,7 +59,48 @@ class NationBuilderSyncFuture:
         self.requests = {"POST": {}, "PUT": {}, "DELETE": {}}
         self.responses = {"POST": {}, "PUT": {}, "DELETE": {}}
 
-    def execute(self):
+    def callbackgen(self, verb):
+        """ Generate callback function to collect responses. """
+
+        # def callback(_, res, err):
+        #     if err:
+        #         raise err
+        #     facebook_id = res["extendedProperties"]["private"]["facebookId"]
+        #     self.responses[verb][facebook_id] = res
+
+        # return callback
+
+    def batchgen(self, method, verb):
+        """ Generate batched requests with callback. """
+        # requests = self.requests[verb]
+        # if any(requests):
+        #     count = 0
+        #     batch = self.calendar.batch(self.callbackgen(verb))
+        #     for req in requests.values():
+        #         self.calendar.logger.info(
+        #             "%s /%s/events/%s", verb, req["calendarId"], req.get("eventId", "")
+        #         )
+        #         batch.add(method(**req))
+        #         count += 1
+        #         if count == MAX_BATCH_REQUESTS:
+        #             count = 0
+        #             yield batch
+        #             batch = self.calendar.batch(self.callbackgen(verb))
+        #     yield batch
+
+    def execbatch(self, method, verb, dryrun=False):
+        """ Execute batches. """
+        # batches = self.batchgen(method, verb)
+        # for batch in batches:
+        #     if dryrun:
+        #         for req in batch._requests.values():
+        #             body = json.loads(req.body)
+        #             fid = body["extendedProperties"]["private"]["facebookId"]
+        #             self.responses[verb][fid] = body
+        #     else:
+        #         batch.execute()
+
+    def execute(self, dryrun=True):
         # Get Google Calendar events
         gcal_events = {x["id"]: x for x in self.gcal_get_events_future.execute()}
 
@@ -70,19 +112,25 @@ class NationBuilderSyncFuture:
         start_times = [
             x["start"]["dateTime"]
             for x in gcal_events.values()
-            if "start" in x and "dateTime" in x["start"]
+            if x.get("start", {}).get("dateTime")
         ]
         end_times = [
             x["end"]["dateTime"]
             for x in gcal_events.values()
-            if "end" in x and "dateTime" in x["end"]
+            if x.get("end", {}).get("dateTime")
         ]
 
-        # address1: Google Calendar event ID
-        # address2: Google Calendar event digest
         nb_events = {
-            x["venue"]["address"]["address1"]: {
-                "digest": x["venue"]["address"]["address2"],
+            next(
+                str.removeprefix(t, EVENT_ID_PREFIX)
+                for t in x["tags"]
+                if str.startswith(EVENT_ID_PREFIX)
+            ): {
+                "digest": next(
+                    str.removeprefix(t, DIGEST_PREFIX)
+                    for t in x["tags"]
+                    if str.startswith(DIGEST_PREFIX)
+                ),
                 "nationbuilder_id": x["id"],
             }
             for x in self.nb.iter_events(
@@ -94,7 +142,7 @@ class NationBuilderSyncFuture:
                     + datetime.timedelta(seconds=1)
                 ),
             )
-            if "gcal_" + self.gcal.calendar_id in x["tags"]
+            if "gcal_id:" + self.gcal.calendar_id in x["tags"]
         }
 
         # Get create/update/delete request payloads
@@ -102,13 +150,13 @@ class NationBuilderSyncFuture:
             digest = utils.digest(event)
             if gcal_id not in nb_events:
                 self.requests["POST"][gcal_id] = {
-                    "body": gcal_event_to_nationbuilder(event, self.gcal.calendar_id),
+                    "body": gcal_to_nb(event, self.gcal.calendar_id),
                 }
 
             elif digest != nb_events[gcal_id]["digest"]:
                 self.requests["PUT"][gcal_id] = {
                     "id": nb_events[gcal_id]["nationbuilder_id"],
-                    "body": gcal_event_to_nationbuilder(event, self.gcal.calendar_id),
+                    "body": gcal_to_nb(event, self.gcal.calendar_id),
                 }
 
         found_ids = {x["id"] for x in nb_events}
@@ -119,4 +167,9 @@ class NationBuilderSyncFuture:
             }
 
         # Execute batched requests
-        pprint(self.requests, width=140)
+        print(json.dumps(self.requests, indent=4))
+
+        # self.execbatch(some_insert_method, "POST", dryrun)
+        # self.execbatch(some_update_method, "PUT", dryrun)
+        # self.execbatch(some_delete_method, "DELETE", dryrun)
+        # return self
--- src/fest_nationbuilder/utils.py
+++ src/fest_nationbuilder/utils.py
@@ -1,14 +1,17 @@
 from fest.utils import digest
 
 
-def gcal_event_to_nationbuilder(event, google_calendar_id):
+def gcal_to_nb(gcal_event, google_calendar_id):
     """ Convert a Google Calendar event to a NationBuilder event. """
     return {
         "status": "unlisted",
-        "name": event["summary"],
-        "excerpt": event.get("description"),
-        "start_time": event["start"]["dateTime"],
-        "end_time": event["end"]["dateTime"],
-        "venue": {"address": {"address1": event["id"], "address2": digest(event)}},
-        "tags": ["gcal_" + google_calendar_id],
+        "name": gcal_event["summary"],
+        "excerpt": gcal_event.get("description"),
+        "start_time": gcal_event["start"]["dateTime"],
+        "end_time": gcal_event["end"]["dateTime"],
+        "tags": [
+            "gcal_id:" + google_calendar_id,
+            "gcal_event_id:" + gcal_event["id"],
+            "gcal_event_digest:" + digest(gcal_event),
+        ],
     }
